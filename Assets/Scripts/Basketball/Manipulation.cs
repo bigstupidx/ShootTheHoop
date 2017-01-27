23b15e7fd997e5155c5d6d18ef25e628b31b035d
Petar <google1999@abv.bg>
2017-01-27 17:39:00 +0200

    Added project files along with the game models and basic shooting functionality

---
+++ Assets/Scripts/Basketball/Manipulation.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manipulation : MonoBehaviour
+{
+    private Rigidbody ballRigidbody;
+    private Transform ballTransform;
+    private float ballVelocity;
+    private Vector3 ballPos;
+    private Vector3 firstMousePos;
+    private Vector3 secondMousePos;
+    private bool ballHasBeenThrown = false;
+    private bool hasCollidedOnce = false;
+    private Vector3[] ballPositions;
+    private Quaternion[] ballRotations;
+    private CameraAngle camAngleScript;
+
+    public float ballAcceleration;
+
+    void Start()
+    {
+        camAngleScript = Camera.main.GetComponent<CameraAngle>();
+        ballRigidbody = GetComponent<Rigidbody>();
+        ballTransform = GetComponent<Transform>();
+        InitializeBallPositionsOnField();
+    }
+
+    void InitializeBallPositionsOnField()
+    {
+        ballPositions = new Vector3[11];
+        ballRotations = new Quaternion[11];
+        ballPositions[0] = new Vector3(-8.33f, 3.66f, -6.25f);
+        ballRotations[0] = Quaternion.Euler(0, 0, 0);
+        ballPositions[1] = new Vector3(-7.68f, 3.66f, -3.22f);
+        ballRotations[1] = Quaternion.Euler(0, 16.717f, 0);
+        ballPositions[2] = new Vector3(-6.3f, 3.66f, -1.001f);
+        ballRotations[2] = Quaternion.Euler(0, 32.085f, 0);
+        ballPositions[3] = new Vector3(-4.28f, 3.66f, 0.54f);
+        ballRotations[3] = Quaternion.Euler(0, 46.491f, 0);
+        ballPositions[4] = new Vector3(-2.06f, 3.66f, 1.92f);
+        ballRotations[4] = Quaternion.Euler(0, 65.314f, 0);
+        ballPositions[5] = new Vector3(-7.84f, 3.66f, -9.23f);
+        ballRotations[5] = Quaternion.Euler(0, -15.787f, 0);
+        ballPositions[6] = new Vector3(1.209f, 3.66f, 3.046f);
+        ballRotations[6] = Quaternion.Euler(0, 86.691f, 0);
+        ballPositions[7] = new Vector3(-6.43f, 3.66f, -11.27f);
+        ballRotations[7] = Quaternion.Euler(0, -30.804f, 0);
+        ballPositions[8] = new Vector3(-4.45f, 3.66f, -13.31f);
+        ballRotations[8] = Quaternion.Euler(0, -48.309f, 0);
+        ballPositions[9] = new Vector3(-1.89f, 3.66f, -14.48f);
+        ballRotations[9] = Quaternion.Euler(0, -66.189f, -0.6950001f);
+        ballPositions[10] = new Vector3(1.865f, 3.66f, -14.88f);
+        ballRotations[10] = Quaternion.Euler(0, -91.96101f, 0);
+    }
+
+    void FixedUpdate()
+    {
+        if (ballHasBeenThrown)
+        {
+            ballTransform.RotateAround(ballTransform.position, ballTransform.forward, Time.deltaTime * 180f);
+        }
+    }
+
+#if UNITY_STANDALONE
+
+    void OnMouseDown()
+    {
+        if (ballRigidbody.isKinematic)
+        {
+            ballRigidbody.isKinematic = false;
+        }
+        firstMousePos = Input.mousePosition;
+        firstMousePos.z = 1;
+        firstMousePos = Camera.main.ScreenToWorldPoint(firstMousePos);
+    }
+
+    void OnMouseDrag()
+    {
+        MakeBallFollowMouse();
+        ChangeBallSpeed();
+    }
+
+    void OnMouseUp()
+    {
+        // Signals the ball has been released applying the accumulated velocity to its rigidbody
+        ballRigidbody.AddRelativeForce(new Vector3(ballVelocity / 2, ballVelocity, 0));
+        ballHasBeenThrown = true;
+        ballVelocity = 0;
+    }
+
+#endif
+
+    void MakeBallFollowMouse()
+    {
+        ballPos = Input.mousePosition;
+        ballPos.z = 1;
+        ballPos = Camera.main.ScreenToWorldPoint(ballPos);
+        ballTransform.position = ballPos;
+    }
+
+    void ChangeBallSpeed()
+    {
+        secondMousePos = Input.mousePosition;
+        secondMousePos.z = 1;
+        secondMousePos = Camera.main.ScreenToWorldPoint(secondMousePos);
+        if (firstMousePos.y < secondMousePos.y)
+        {
+            ballVelocity += ballAcceleration;
+            if (ballVelocity > 545)
+            {
+                ballVelocity = 545;
+            }
+        }
+        else if (firstMousePos == secondMousePos)
+        {
+            if (ballVelocity - ballAcceleration * 20 >= 0)
+            {
+                ballVelocity -= ballAcceleration * 20;
+            }
+            else if (ballVelocity != 0)
+            {
+                ballVelocity = 0;
+            }
+        }
+        firstMousePos = secondMousePos;
+        Debug.Log(ballVelocity);
+    }
+
+    void OnCollisionEnter()
+    {
+        if(!hasCollidedOnce) {
+            ballHasBeenThrown = false;
+            int randIndex = Random.Range(0, 11);
+            GenerateNewBallWithPositionIndex(randIndex);
+            camAngleScript.ChangeAngle(randIndex);
+            DeleteThrownBall();
+            hasCollidedOnce = true;
+        } 
+    }
+
+    void GenerateNewBallWithPositionIndex(int index)
+    {
+        GameObject newBall;
+        newBall = Instantiate(gameObject, ballPositions[index], ballRotations[index]) as GameObject;
+        newBall.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void DeleteThrownBall()
+    {
+        Destroy(gameObject, 4f);
+    }
+}
---
+++ Assets/Scripts/Camera/CameraAngle.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAngle : MonoBehaviour {
+
+    private Vector3[] camAnglesPositions;
+    private Quaternion[] camAnglesRotations;
+    private Transform camTransform;
+
+    void Start()
+    {
+        camTransform = GetComponent<Transform>();
+        InitializeCameraAngles();
+    }
+
+    void InitializeCameraAngles()
+    {
+        camAnglesPositions = new Vector3[11];
+        camAnglesRotations = new Quaternion[11];
+        camAnglesPositions[0] = new Vector3(-9.12f, 4.17f, -6.26f);
+        camAnglesRotations[0] = Quaternion.Euler(0, 90, 0);
+        camAnglesPositions[1] = new Vector3(-8.434f, 4.17f, -2.955f);
+        camAnglesRotations[1] = Quaternion.Euler(0, 107.11f, 0);
+        camAnglesPositions[2] = new Vector3(-6.955f, 4.17f, -0.521f);
+        camAnglesRotations[2] = Quaternion.Euler(0, 126.043f, 0);
+        camAnglesPositions[3] = new Vector3(-4.807f, 4.17f, 1.138f);
+        camAnglesRotations[3] = Quaternion.Euler(0, 137.096f, 0);
+        camAnglesPositions[4] = new Vector3(-2.334f, 4.17f, 2.529f);
+        camAnglesRotations[4] = Quaternion.Euler(0, 158.3f, 0);
+        camAnglesPositions[5] = new Vector3(-8.582f, 4.17f, -9.473f);
+        camAnglesRotations[5] = Quaternion.Euler(0, 72.24001f, 0);
+        camAnglesPositions[6] = new Vector3(1.177f, 4.17f, 3.785f);
+        camAnglesRotations[6] = Quaternion.Euler(0, 176.601f, 0);
+        camAnglesPositions[7] = new Vector3(-7.09f, 4.17f, -11.69f);
+        camAnglesRotations[7] = Quaternion.Euler(0, 57.959f, 0);
+        camAnglesPositions[8] = new Vector3(-4.94f, 4.17f, -13.91f);
+        camAnglesRotations[8] = Quaternion.Euler(0, 39.908f, 0);
+        camAnglesPositions[9] = new Vector3(-2.2f, 4.17f, -15.19f);
+        camAnglesRotations[9] = Quaternion.Euler(0, 22.622f, 0);
+        camAnglesPositions[10] = new Vector3(1.9f, 4.17f, -15.68f);
+        camAnglesRotations[10] = Quaternion.Euler(0, 0.205f, 0);
+    }
+
+    public void ChangeAngle(int index)
+    {
+        camTransform.position = camAnglesPositions[index];
+        camTransform.rotation = camAnglesRotations[index];
+    }
+}
